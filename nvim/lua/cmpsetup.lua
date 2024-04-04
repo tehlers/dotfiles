27ff4327fd99d49fddc38c85a0a6292492e3f154
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2024-04-04 16:15:47 +0200

    Remove superfluous lspconfig setup

--- nvim/lua/cmpsetup.lua
+++ nvim/lua/cmpsetup.lua
@@ -58,10 +58,3 @@ cmp.setup.cmdline(':', {
   }),
   matching = { disallow_symbol_nonprefix_matching = false }
 })
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  capabilities = capabilities
-}
